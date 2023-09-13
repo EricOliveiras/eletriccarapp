d5a6bb8c1c17e989ae7f4f17b057a16cd704ca3f
Eric Oliveira <oiericoi@hotmail.com>
2023-09-13 13:26:39 -0300

    feat: add new funcs in main

--- app/src/main/java/com/example/eletriccarapp/MainActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.eletriccarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var price: EditText
+    private lateinit var km: EditText
+    private lateinit var result: TextView
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupView()
+        setupListenner()
+    }
+
+    private fun setupView() {
+        price = findViewById<EditText>(R.id.edit_kwh)
+        km = findViewById<EditText>(R.id.km_percorrido)
+        result = findViewById<TextView>(R.id.tv_result)
+        button = findViewById<Button>(R.id.calc_btn)
+    }
+
+    private fun setupListenner() {
+        button.setOnClickListener {
+            calcular()
+        }
+    }
+
+    private fun calcular() {
+        val priceValue = price.text.toString().toFloat()
+        val kmValue = km.text.toString().toFloat()
+
+        val sum = priceValue / kmValue
+
+        result.visibility = View.VISIBLE
+        result.text = sum.toString()
     }
 }
